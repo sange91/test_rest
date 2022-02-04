ff0d619dff78bfe51a8166beb18f4b9a49f6e9d5
Sange Lama <sangelama91@hotmail.com>
2022-02-04 14:04:24 +0530

    tes

---
+++ deps/beanie/__init__.py
@@ -0,0 +1,45 @@
+from beanie.migrations.controllers.free_fall import free_fall_migration
+from beanie.migrations.controllers.iterative import iterative_migration
+from beanie.odm.actions import (
+    before_event,
+    after_event,
+    Insert,
+    Replace,
+    SaveChanges,
+    ValidateOnSave,
+)
+from beanie.odm.bulk import BulkWriter
+from beanie.odm.fields import (
+    PydanticObjectId,
+    Indexed,
+    Link,
+    WriteRules,
+    DeleteRules,
+)
+from beanie.odm.utils.general import init_beanie
+from beanie.odm.documents import Document
+
+__version__ = "1.8.12"
+__all__ = [
+    # ODM
+    "Document",
+    "init_beanie",
+    "PydanticObjectId",
+    "Indexed",
+    # Actions
+    "before_event",
+    "after_event",
+    "Insert",
+    "Replace",
+    "SaveChanges",
+    "ValidateOnSave",
+    # Bulk Write
+    "BulkWriter",
+    # Migrations
+    "iterative_migration",
+    "free_fall_migration",
+    # Relations
+    "Link",
+    "WriteRules",
+    "DeleteRules",
+]
